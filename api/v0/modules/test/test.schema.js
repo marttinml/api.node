5fd818b624d78932d9953a0c0098c298e46af90d
Martín Mtz <martin.ml@icloud.com>
2017-01-25 23:43:56 -0600

    schema

--- api/v0/modules/test/test.schema.js
+++ api/v0/modules/test/test.schema.js
@@ -4,10 +4,5 @@ module.exports = {
 		testId 			: { type: "number" },
 		name 			: { type: "string", required:true },
 		description 	: { type: "string", required:true },
-		data 			: { type: "object", required: true,
-			properties 	: {
-				id: { type: "number", required: true }
-			}
-		}
 	}
 };
