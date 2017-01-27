efba502fb1c87859f3d634c85cdb88bae3870160
Martín Mtz <martin.ml@icloud.com>
2017-01-27 15:07:53 -0600

    left TZ

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var MongoDB = require('./api/v0/config/api.mongodb'),
     express  = require('./api/v0/config/api.express'),
     app      = express();
 
-process.env.TZ = 'America/Los_Angeles';
-
 var port = process.env.PORT || 2000;
 app.listen(port, function () {
     console.log("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
