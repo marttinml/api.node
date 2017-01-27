5f5e8670b0df22e9dac14fa7ba0383b8b02ec9ad
Martín Mtz <martin.ml@icloud.com>
2017-01-27 12:53:48 -0600

    Owner

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ process.env.TZ = 'America/Los_Angeles';
 var port = process.env.PORT || 2000;
 app.listen(port, function () {
     console.log("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-    console.log(" |     API REST [api.node] - http://localhost:" + port + "   | ");
+    console.log(" |     API REST [api.node] - http://localhost:" + port + "     | ");
     console.log(" - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+    console.log("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+    console.log(" |     Owner: Martín Mtz 			       | ");
+    console.log(" - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+    console.log("\n");
+    console.log("\n");
 });
